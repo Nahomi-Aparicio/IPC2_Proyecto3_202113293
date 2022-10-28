e4ef33c8cbfb79f42c06bf2cdc2e4cd738729e2b
Nahomi-Aparicio <3072986550401@ingenieria.usac.edu.gt>
2022-10-28 13:16:35 -0600

    se añadio base de datos

--- Backend/app.py
+++ Backend/app.py
@@ -3,6 +3,7 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 import xml.etree.ElementTree as ET
 from datos import XML
+import json
 
 leei=XML()
 
@@ -16,6 +17,7 @@ Dato=[{'nombre':'pan','apellido':'juan'},{'nombre':'realizado','apellido':'gato'
 
 @app.route('/consultaDatos', methods=['GET'])
 def obteniendoDato():
+
     return jsonify(Dato)
 
 
@@ -23,15 +25,33 @@ def obteniendoDato():
 def creandoDato():
     body= request.get_json()
 
-    
     apellido=body['apellido']
-  
-
     return jsonify({'msg':'Dato agregado'})
-#---------------------------------------------------------------------------------------------------
-"""""@app.route('/consultaDatos', methods=['GET'])
-def consultaDatos():
-    return jsonify('holis datos')"""""
+
+#-------------------------------------------------------CONSULTA DATOS------------------------------------------------
+@app.route('/consultandoDatos', methods=['GET'])
+def obteniendoDatos():
+    with open('recursosJson.json', "r") as file:
+       data = json.load(file)
+
+    with open('categoriaJson.json', "r") as file:
+         data2 = json.load(file)
+        
+    with open('cateconfigJson.json', "r") as file:
+            data3 = json.load(file)
+    
+    with open('clienteJSon.json', "r") as file:
+       data4 = json.load(file)
+
+    with open('instanciaJson.json', "r") as file:
+         data5 = json.load(file)
+
+
+    return jsonify(data,data2,data3,data4,data5)
+
+
+
+#--------------------------------------CARGAR DATOS DEL XML------------------------------------------------------------
 
 @app.route('/cargarDatos', methods=["POST"])
 def cargarDatos():
@@ -47,27 +67,132 @@ def cargarDatos2():
     leei.entrada2lectu(parse_request2)
     return (parse_request2)
 
-
+#--------------------------------------------------RECURSOS--------------------------------------------------------
 @app.route('/crearRecurso', methods=['POST'])
 def crearRecurso():
-    return jsonify('holis recursos')
+    body= request.get_json()
+    nit=body['id']
+    nombre=body['nombre']
+    abreviatura=body['abreviatura']
+    metrica=body['metrica']
+    tipo=body['tipo']
+    valorXhora=body['valorXhora']
+    with open('recursosJson.json', "r") as file:
+       data = json.load(file)
+    data['recursos'].append({'id':nit,'nombre': nombre,'abreviatura': abreviatura,'metrica': metrica,'tipo':tipo,'valorXhora': valorXhora})
+    with open('recursosJson.json', "w") as file:
+         json.dump(data, file)
+    Dato = {
+                'message': 'Recurso agregado Exitosamente',
+                'state':200
+            }
+    return jsonify(Dato)
+
+@app.route('/crearRecurso', methods=['GET'])
+def getRecurso():
+    with open('recursosJson.json', "r") as file:
+       data = json.load(file)    
+    return jsonify(data)
+#-----------------------------------------------------CATEGORIAS------------------------------------
 
 @app.route('/crearCategoria', methods=['POST'])
 def crearCategoria():
-    return jsonify('holis recursos')
+    body2= request.get_json()
+    nit=body2['id']
+    nombre=body2['nombre']
+    descripcion=body2['descripcion']
+    cargaTrabajo=body2['cargaTrabajo']
+    with open('categoriaJson.json', "r") as file:
+         data2 = json.load(file)
+    data2['categoria'].append({'id':nit,'nombre': nombre,'descripcion': descripcion,'cargaTrabajo': cargaTrabajo})
+    with open('categoriaJson.json', "w") as file:
+            json.dump(data2, file)
+    Datos = {
+                'message': 'Categoria agregada Exitosamente',
+                'state':200
+            }
+    return jsonify(Datos)
+    
+@app.route('/crearCategoria', methods=['GET'])
+def getCategoria():
+    with open('categoriaJson.json', "r") as file:
+       data2 = json.load(file)    
+    return jsonify(data2)
 
-
+#-----------------------------------------------------CONFIGURACION------------------------------------
 @app.route('/crearConfiguracion', methods=['POST'])
 def crearConfiguracion():
-    return jsonify('holis configuracion')
+    body3= request.get_json()
+    Id=body3['ID']
+    nit=body3['id']
+    nombre=body3['nombre']
+    descripcion=body3['descripcion']
+    idRC=body3['idR']
+    cantidad=body3['Recursos']
+    with open('cateconfigJson.json', "r") as file:
+            data3 = json.load(file)
+    data3['configuracion'].append({'ID':Id,'id':nit,'nombre': nombre,'descripcion': descripcion,'idR': idRC,'Recursos':cantidad})
+    with open('cateconfigJson.json', "w") as file:
+            json.dump(data3, file)
+    Datos = {
+                'message': 'Configuracion agregada Exitosamente',
+                'state':200
+            }
+    return jsonify(Datos)
+    
+@app.route('/crearConfiguracion', methods=['GET'])
+def getConfiguracion():
+    with open('cateconfigJson.json', "r") as file:
+       data3 = json.load(file)    
+    return jsonify(data3)
 
+#-----------------------------------------------------CREARCLIENTE------------------------------------
 @app.route('/crearCliente', methods=['POST'])
 def crearCliente():
-    return jsonify('holis cliente')
+    body4= request.get_json()
+    nit=body4['nit']
+    nombre=body4['nombre']
+    usuario=body4['usuario']
+    direccion=body4['direccion']
+    correoElectronico=body4['correoElectronico']
+    with open ('clienteJSon.json', "r") as file:
+        data4 = json.load(file)
+    data4['cliente'].append({'nit':nit,'nombre': nombre,'usuario': usuario,'direccion': direccion,'correoElectronico': correoElectronico})
+    with open('clienteJSon.json', "w") as file:
+            json.dump(data4, file)
+    Datos = {
+                'message': 'Cliente agregado Exitosamente',
+                'state':200
+            }
+    return jsonify(Datos)
 
+@app.route('/crearCliente', methods=['GET'])
+def getCliente():
+    with open('clienteJSon.json', "r") as file:
+       data4 = json.load(file)    
+    return jsonify(data4)
+    
+#-----------------------------------------------------------------------------------------
 @app.route('/crearInstancia', methods=['POST'])
 def crearInstancia():
-    return jsonify('holis instancia')
+    body5= request.get_json()
+    id=body5['id']
+    idConfiguracion=body5['idConfiguracion']
+    nombre=body5['nombre']
+    fechaInicio=body5['fechaInicio']
+    estado=body5['estado']
+    fechaFinal=body5['fechaFinal']
+    with open ('instanciaJson.json', "r") as file:
+        data5 = json.load(file)
+    data5['instancia'].append({'id':id,'idConfiguracion': idConfiguracion,'nombre': nombre,'fechaInicio': fechaInicio,'estado': estado,'fechaFinal': fechaFinal})
+    with open('instanciaJson.json', "w") as file:
+            json.dump(data5, file)
+    Datos = {
+                'message': 'Instancia agregada Exitosamente',
+                'state':200
+            }
+    return jsonify(Datos)
+
 
 @app.route('/crearFactura', methods=['POST'])
 def crearFactura():
--- Backend/datos.py
+++ Backend/datos.py
@@ -1,26 +1,56 @@
-import xml.etree.ElementTree as ET
+from xml.dom.minidom import *
 import re
-
+from datetime import datetime
+import json
 from xml.dom import minidom as MD
 
 class XML():
-    
+    global recursosJson
+    global categoriaJson
+    global cateconfigJson
+    global  clienteJSon
+    global  instanciaJson
+
+    recursosJson={}
+    recursosJson['recursos']=[]
+
+    categoriaJson={}
+    categoriaJson['categoria']=[]
+
+    cateconfigJson={}
+    cateconfigJson['configuracion']=[]
+
+    clienteJSon={}
+    clienteJSon['cliente']=[]
+
+    instanciaJson={}
+    instanciaJson['instancia']=[]
+   
+
     def entrada(self,ruta):
           
         #file = open(ruta,'r', encoding="utf-8")
         # print(ruta)
+       
+        listaI=[]
+        listaR=[]
         xmldoc = MD.parse(ruta) 
         ruta2= xmldoc.documentElement
         listandoR= ruta2.getElementsByTagName("listaRecursos")[0]
         recursos = listandoR.getElementsByTagName("recurso")
+
+#recursos---------------------------------------------------------------------
+
         for recurso in recursos:
             idR=recurso.getAttribute("id")
             nombreR=recurso.getElementsByTagName("nombre")[0].firstChild.data
             abrev=recurso.getElementsByTagName("abreviatura")[0].firstChild.data
             metric=recurso.getElementsByTagName("metrica")[0].firstChild.data
+            tipo=recurso.getElementsByTagName("tipo")[0].firstChild.data
             valorXh=recurso.getElementsByTagName("valorXhora")[0].firstChild.data
             # aqui separar dato 
-
+            recursosJson['recursos'].append({'id':idR,'nombre': nombreR,'abreviatura': abrev,'metrica': metric,'tipo':tipo,'valorXhora': valorXh})
+# categorias ------------------------------------------------------------
         listandoC= ruta2.getElementsByTagName("listaCategorias")[0]
         categoria=listandoC.getElementsByTagName("categoria")
         for cat in categoria:
@@ -30,20 +60,32 @@ class XML():
             cargaTrab=cat.getElementsByTagName("cargaTrabajo")[0].firstChild.data
             configu=cat.getElementsByTagName("configuracion")
             #print(idC, nombreC, descrip, cargaTrab)
-            # aqui separar dato 
+            categoriaJson['categoria'].append({'id':idC,'nombre': nombreC,'descripcion': descrip,'cargaTrabajo': cargaTrab})
+# configuracion------------------------------------------------------------ 
             for confi in configu:
                 idConf=confi.getAttribute("id")
                 nombreConf=confi.getElementsByTagName("nombre")[0].firstChild.data
                 descripcionConf=confi.getElementsByTagName("descripcion")[0].firstChild.data
                 recurConfig=confi.getElementsByTagName("recurso")
                 #print(idConf, nombreConf, descripcionConf)
-                # aqui separar dato 
+                # aqui separar dato
+
                 for recurC in recurConfig:
                     idRC=recurC.getAttribute("id")
                     cantidadRecurC=recurC.firstChild.data
-                    #print(idRC,cantidadRecurC)
-                    # aqui separar dato 
-        
+                    listaI.append(idRC)
+                    listaR.append(cantidadRecurC)
+
+                    
+                print(listaI)
+                print(listaR)
+                    # aqui separar dato Backend 
+                cateconfigJson['configuracion'].append({'ID':idC,'id':idConf,'nombre':nombreConf,'descripcion':descripcionConf,'idR': listaI,'Recursos': listaR})
+                listaR=[]
+                listaI=[]
+
+
+#lista clientes-------------------------------------
         listandoCl= ruta2.getElementsByTagName("listaClientes")[0]
         cliente=listandoCl.getElementsByTagName("cliente")
         for cli in cliente:
@@ -53,9 +95,12 @@ class XML():
             direcCl=cli.getElementsByTagName("direccion")[0].firstChild.data
             correoCl=cli.getElementsByTagName("correoElectronico")[0].firstChild.data
             inst=cli.getElementsByTagName("instancia")
-            #print(idCl, nombreCl, usuaCl, direcCl, correoCl)
-            # aqui separar dato
+            #print(idCl, nombreCl, usuaCl, direcCl, correoCl)  instanciaJson
+            clienteJSon['cliente'].append({'nit':idCl,'nombre': nombreCl,'usuario': usuaCl,'direccion': direcCl,'correoElectronico': correoCl})
+            
+#instancua datos----------------------------------------------------------
             for insta in inst:
+            
                 idInst=insta.getAttribute("id")
                 idConf=insta.getElementsByTagName("idConfiguracion")[0].firstChild.data
                 nombreInst=insta.getElementsByTagName("nombre")[0].firstChild.data
@@ -64,7 +109,27 @@ class XML():
                 if estInst == "Cancelada":
                     fechafinInst=insta.getElementsByTagName("fechaFinal")[0].firstChild.data
                 else:
-                    fechafinInst="---"
+                    fechafinInst="---"                
+                instanciaJson['instancia'].append({'id': idInst,'idConfiguracion': idConf,'nombre': nombreInst,'fechaInicio': fechaInst,'estado': estInst,'fechaFinal': fechafinInst})
+
+        with open('recursosJson.json', 'w+') as file:
+            json.dump(recursosJson, file, indent=4)
+
+        with open('categoriaJson.json', 'w+') as file:
+            json.dump(categoriaJson, file, indent=4)
+        
+        with open('cateconfigJson.json', 'w+') as file:
+            json.dump(cateconfigJson, file, indent=4)
+
+        with open('instanciaJson.json', 'w+') as file:
+            json.dump(instanciaJson, file, indent=4)
+
+        with open('clienteJSon.json', 'w+') as file:
+            json.dump(clienteJSon, file, indent=4)
+
+
+
+
 
     def entrada2lectu(self, ruta2):
         xmldoc = MD.parse(ruta2) 
